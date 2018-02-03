01c74ca23147927a9a38399f905b3ed45b64f504
mediastore93 <mediastore93@gmail.com>
2018-02-03 12:15:37 -0800

    Update test.py

--- test.py
+++ test.py
@@ -36,38 +36,4 @@ def tree():
 
 while True:
     tree()
-    sleep(1)tetime
-import time
-import unicornhat as unicorn
-
-def binary():
-  
-  decimal = time.strftime("%H%M%S")
-  decimal_list = list(decimal)
-
-  for x in xrange(0, 6):
-    binary = bin(int(decimal_list[x]))[2:].rjust(4, '0')
-    binary_list = list(binary)
-
-    for y in xrange(0, 4):
-      if binary_list[y] == '1':
-        unicorn.set_pixel(x+1,y,255,255,255)
-      else:
-        unicorn.set_pixel(x+1,y,0,0,0)
-
-def tree():
-    timestamp = datetime.datetime.now().time()
-    start = datetime.time(0, 1)
-    end = datetime.time(12, 0)
-    print(str(timestamp))
-
-    if start <= timestamp <= end:
-        print('hello Remi')
-        binary()
-    if timestamp >= end:
-        print('break loop')
-        
-
-while True:
-    tree()
     sleep(1)
